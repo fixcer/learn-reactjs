d271c130a6bd8a37555a3ab51b4ee4117d27ab17
fixcer <toan@fixcer.me>
2020-10-04 20:10:44 +0700

    API

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,55 +1,21 @@
 import React from 'react';
-import { Field, reduxForm } from 'redux-form';
-
-const validate = (formValues) => {
-  const errors = {};
-
-  if (!formValues.title) {
-    errors.title = 'You must enter a title';
-  }
-
-  if (!formValues.description) {
-    errors.description = 'You must enter a description';
-  }
-
-  return errors;
-};
-
-const renderInput = ({ input, label, meta }) => {
-  const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
-
-  return (
-    <div className={className}>
-      <label>{label}</label>
-      <input {...input} autoComplete='off' />
-      {meta.error && meta.touched ? (
-        <div className='ui error message'>
-          <div className='header'>{meta.error}</div>
-        </div>
-      ) : null}
-    </div>
-  );
-};
+import { connect } from 'react-redux';
+import { createStream } from '../../actions/index';
+import StreamForm from './StreamForm';
 
 const StreamCreate = (props) => {
+  const { createStream } = props;
+
   const onSubmit = (formValues) => {
-    console.log(formValues);
+    createStream(formValues);
   };
 
   return (
-    <form className='ui form error' onSubmit={props.handleSubmit(onSubmit)}>
-      <Field name='title' component={renderInput} label='Enter Title' />
-      <Field
-        name='description'
-        component={renderInput}
-        label='Enter Description'
-      />
-      <button className='ui button primary'>Submit</button>
-    </form>
+    <>
+      <h3>Create a Stream</h3>
+      <StreamForm onSubmit={onSubmit} />
+    </>
   );
 };
 
-export default reduxForm({
-  form: 'streamCreate',
-  validate,
-})(StreamCreate);
+export default connect(null, { createStream })(StreamCreate);
--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { editStream, fetchStream } from '../../actions';
+import StreamForm from './StreamForm';
 
-const StreamEdit = () => {
-  return <div></div>;
+const StreamEdit = (props) => {
+  const { editStream, fetchStream, match, stream } = props;
+
+  const onSubmit = (formValues) => {
+    editStream(match.params.id, formValues);
+  };
+
+  useEffect(() => {
+    fetchStream(match.params.id);
+    // eslint-disable-next-line
+  }, []);
+
+  if (!stream) {
+    return <div>Loading...</div>;
+  }
+
+  const { title, description } = stream;
+
+  return (
+    <>
+      <h3>Edit a Stream</h3>
+      <StreamForm initialValues={{ title, description }} onSubmit={onSubmit} />
+    </>
+  );
 };
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { editStream, fetchStream })(
+  StreamEdit
+);
