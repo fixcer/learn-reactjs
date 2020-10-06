375d80d576dab22a14b1b4facf1c912c0c35df1a
fixcer <toan@fixcer.me>
2020-10-06 21:14:22 +0700

    RMTP

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { connect } from 'react-redux';
+import flv from 'flv.js';
+import { fetchStream } from '../../actions';
 
-const StreamShow = () => {
-  return <div></div>;
+const StreamShow = ({ match, fetchStream, stream }) => {
+  const videoRef = useRef();
+  const player = useRef(null);
+
+  const buildPlayer = () => {
+    const { id } = match.params;
+
+    if (player.current || !stream) {
+      return;
+    }
+
+    if (stream && flv.isSupported()) {
+      player.current = flv.createPlayer({
+        type: 'flv',
+        url: `http://localhost:8000/live/${id}.flv`,
+      });
+      player.current.attachMediaElement(videoRef.current);
+      player.current.load();
+    }
+  };
+
+  useEffect(() => {
+    const { id } = match.params;
+    fetchStream(id);
+    buildPlayer();
+
+    // eslint-disable-next-line
+  }, [match]);
+
+  useEffect(() => {
+    buildPlayer();
+
+    return function cleanUp() {
+      if (player.current) {
+        player.current.destroy();
+      }
+    };
+
+    // eslint-disable-next-line
+  }, [match, stream]);
+
+  if (!stream) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <video ref={videoRef} style={{ width: '100%' }} controls></video>
+      <h1>{stream.title}</h1>
+      <h5>{stream.description}</h5>
+    </div>
+  );
 };
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
